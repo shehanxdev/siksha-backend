5876ee37b38786b043d35e9c322a4157da6ab8f7
Shehan Chanuka <shehanxdev@gmail.com>
2023-04-15 20:27:51 +0530

    Initial commit

---
+++ src/test/java/com/HEC/siksha/SikshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HEC.siksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
